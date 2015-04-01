53978b6687ef69277547a483eee50cbe29c5d7fb
Faith <fachan@calpoly.edu>
2015-04-01 12:18:15 -0700

    Converted functions (can_move_horizontal, can_move_vertical, move_ball) from main.py to methods in Ball class (ball.py)

--- lab1/methods/main.py
+++ lab1/methods/main.py
@@ -25,11 +25,10 @@ def draw(screen, balls) :
    for ball in balls:
       ball.draw_ball(screen)
 
-
+      '''
 def can_move_horizontal(ball, width):
    return ((ball.dx > 0 and ball.x + ball.radius + ball.dx < width) or
          (ball.dx < 0 and ball.x - ball.radius + ball.dx >= 0))
-
 
 def can_move_vertical(ball, height):
    return ((ball.dy > 0 and ball.y + ball.radius + ball.dy < height) or
@@ -43,12 +42,12 @@ def move_ball(ball, width, height, balls):
       ball.dy *= -1
    ball.x += ball.dx
    ball.y += ball.dy
-
+   '''
 
 def move(balls, width, height):
    for ball in balls:
-      move_ball(ball, width, height, balls)
-
+      ball.move_ball(width, height, balls)
+      
 
 def event_loop(screen, balls, width, height):
    while True:
